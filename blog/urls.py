b8c65c118726c8a791c1a51b798269a704f578c0
cvam0000 <cvam0000@gmail.com>
2019-06-03 03:26:12 +0530

    EfeF

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='blog'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     data = Post.objects.all()
@@ -10,3 +11,9 @@ def post_list(request):
     dataBlog = {"blog": data }
     
     return render(request, 'blog/blog.html', dataBlog )
+
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post.html', {'post': post})    
